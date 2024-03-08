94104fbc18d052163c84166110dda38badf97dc4
prasad-gurav <psgurav2001@gmail.com>
2024-03-08 18:06:24 +0530

    basic changed

--- src/pages/FavouritePage.jsx
+++ src/pages/FavouritePage.jsx
@@ -8,17 +8,21 @@ export default function FavouritePage() {
         <div className='h-[90vh] text-white'>
             <div className='flex flex-warp justify-center gap-4 m-2 p-2'>
                 {
-                    favourites.map(movie => (
-                        <Link className='w-60' to={`/movie/${movie.id}`} >
-                            <div className="">
-                                <img src={`https://image.tmdb.org/t/p/original${movie && movie.movie_poster}`} />
-                            </div>
-                            <div className="">
-                                <div className="text-xl">{movie ? movie.movie_name : ""}</div>
+                    favourites.length > 0 ?
+                        favourites.map(movie => (
+                            <Link className='w-60' to={`/movie/${movie.id}`} >
+                                <div className="">
+                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.movie_poster}`} />
+                                </div>
+                                <div className="">
+                                    <div className="text-xl">{movie ? movie.movie_name : ""}</div>
 
-                            </div>
-                        </Link>
-                    ))
+                                </div>
+                            </Link>
+                        )) :
+                        <>
+                            <Link className='px-2 py-1 text-md bg-yellow-500 text-black rounded-md' to={'/'}>Add Some To Favourites</Link>
+                        </>
                 }
             </div>
         </div>
--- src/pages/WatchListPage.jsx
+++ src/pages/WatchListPage.jsx
@@ -7,17 +7,22 @@ export default function WatchListPage() {
         <div className='h-[90vh] text-white'>
             <div className='flex flex-warp justify-center gap-4 m-2 p-2'>
                 {
-                    watchlist.map(movie => (
-                        <Link className='w-60'  to={`/movie/${movie.id}`} >
-                            <div className="">
-                                <img src={`https://image.tmdb.org/t/p/original${movie && movie.movie_poster}`} />
-                            </div>
-                            <div className="">
-                                <div className="text-xl">{movie ? movie.movie_name : ""}</div>
+                    watchlist.length > 0 ?
+                        watchlist.map(movie => (
+                            <Link className='w-60' to={`/movie/${movie.id}`} >
+                                <div className="">
+                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.movie_poster}`} />
+                                </div>
+                                <div className="">
+                                    <div className="text-xl">{movie ? movie.movie_name : ""}</div>
 
-                            </div>
-                        </Link>
-                    ))
+                                </div>
+                            </Link>
+                        ))
+                        :
+                        <>
+                            <Link className='px-2 py-1 text-md bg-red-500 text-black rounded-md' to={'/'}>Add Some To Watchlist</Link>
+                        </>
                 }
             </div>
         </div>
